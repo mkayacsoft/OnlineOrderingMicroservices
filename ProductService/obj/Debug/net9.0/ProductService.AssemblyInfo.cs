68761a6d4e7c1cc9870d8eb7603852030d501fb1
mehmetkayaci <mehmetkycbusiness@gmail.com>
2025-04-29 09:26:32 +0300

    OrderService created

--- ProductService/obj/Debug/net9.0/ProductService.AssemblyInfo.cs
+++ ProductService/obj/Debug/net9.0/ProductService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+288eb9335e4ffbd0743dbc539b7b07a68e41d0f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
